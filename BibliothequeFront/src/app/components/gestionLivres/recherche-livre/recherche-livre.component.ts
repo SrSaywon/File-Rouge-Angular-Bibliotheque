1ad4bb707ed185d27b1c5bd27a45a29a9354da65
SrSaywon <ts.fremaux@gmail.com>
2024-02-12 14:24:17 +0100

    Ajout du HTML de la barre de recherche

--- BibliothequeFront/src/app/components/gestionLivres/recherche-livre/recherche-livre.component.ts
+++ BibliothequeFront/src/app/components/gestionLivres/recherche-livre/recherche-livre.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-recherche-livre',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './recherche-livre.component.html',
   styleUrl: './recherche-livre.component.css'
 })
 export class RechercheLivreComponent {
+
+
+
+  search() {
+
+  }
 
 }
--- BibliothequeFront/src/app/environnement/environnement.ts
+++ BibliothequeFront/src/app/environnement/environnement.ts
@@ -1,4 +1,4 @@
 export const environment = {
     // Careful, must NOT end with /
-    BASE_API_URL: ''
+    BASE_API_URL: 'bibliotheque-poei-4.azurewebsites.net/api'
 };
