fa90bb7b0ea710a047edf4fc40858e8effe05f9b
JessicaDaRosa <jess_GRosa@hotmail.com>
2024-02-13 09:58:44 +0100

    auteurs a moitiée

--- BibliothequeFront/src/app/app.config.ts
+++ BibliothequeFront/src/app/app.config.ts
@@ -1,9 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { BASE_API_URL } from './constants/injection';
+import { environment } from './environnement/environnement';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideHttpClient(),
+    {
+    provide: BASE_API_URL,
+    useValue: environment.BASE_API_URL
+  }]
 };
