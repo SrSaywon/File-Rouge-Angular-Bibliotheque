38b61e26e628f54e12d834bbf7f4659aaf895392
SrSaywon <ts.fremaux@gmail.com>
2024-02-14 13:55:29 +0100

    affichage liste auteur et domaine dans ajouterLivre

--- BibliothequeFront/src/app/components/gestionLivres/ajouter-livres/ajouter-livres.component.ts
+++ BibliothequeFront/src/app/components/gestionLivres/ajouter-livres/ajouter-livres.component.ts
@@ -4,21 +4,31 @@ import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { Router } from '@angular/router';
 import { MatButton, MatFabButton } from '@angular/material/button';
-import { Auteur } from '../../../Models/auteur';
 import { MatIcon } from '@angular/material/icon';
 import { LivreService } from '../../../services/livre.service';
+import {MatSelectModule} from '@angular/material/select';
+import { AuteurService } from '../../../services/auteur.service';
+import { DomaineService } from '../../../services/domaine.service';
+import { Observable } from 'rxjs';
+import { Domaine } from '../../../Models/domaine';
+import { Auteur } from '../../../Models/auteur';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-ajouter-livres',
   standalone: true,
-  imports: [MatInputModule,MatFormFieldModule, MatButton, FormsModule, ReactiveFormsModule, MatButton, MatIcon, MatFabButton],
+  imports: [MatInputModule,MatFormFieldModule, MatButton, FormsModule, ReactiveFormsModule, MatButton, MatIcon, MatFabButton, MatSelectModule, AsyncPipe, NgFor, NgIf],
   templateUrl: './ajouter-livres.component.html',
   styleUrl: './ajouter-livres.component.css'
 })
 export class AjouterLivresComponent {
 
-  constructor(private route: Router, private livreService: LivreService) {}
+  constructor(private route: Router, private livreService: LivreService, private auteurService: AuteurService, private domaineService: DomaineService) {}
 
+  domaines$: Observable<Domaine[]> = this.domaineService.getAll();
+  auteurs$: Observable<Auteur[]> = this.auteurService.getAll();
+
+  selected = '';
   
   ajouterLivresForm = new FormGroup({
     titre: new FormControl(''),
