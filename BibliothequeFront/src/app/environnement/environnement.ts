9614bf1dd30910c1338516914a88f69c96321203
PierreIED <95627325+PierreIED@users.noreply.github.com>
2024-02-14 10:24:41 +0100

    changed Base URL for api

--- BibliothequeFront/src/app/environnement/environnement.ts
+++ BibliothequeFront/src/app/environnement/environnement.ts
@@ -1,4 +1,4 @@
 export const environment = {
     // Careful, must NOT end with /
-    BASE_API_URL: 'https://bibliotheque-poei-4.azurewebsites.net/api'
+    BASE_API_URL: 'https://bibliotheque-groupe4.azurewebsites.net/api'
 };
